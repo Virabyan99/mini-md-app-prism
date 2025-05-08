34ba7d7a3ef9aa3462bba5d1440d90ff0c096d83
Virabyan99 <gmparstone99@gmail.com>
2025-05-08 12:51:44 +0400

    controls removed from abc

--- lib/prism.ts
+++ lib/prism.ts
@@ -14,6 +14,7 @@ import 'prismjs/components/prism-powershell';
 import 'prismjs/components/prism-diff';
 import 'prismjs/components/prism-git';
 import 'prismjs/components/prism-mermaid';
+import 'prismjs/components/prism-latex';
 // import 'prismjs/components/prism-php';
 import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-ruby';
